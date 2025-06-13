2f5afd3daf8fed6a23cc7730cefc73c22e20c87a
janakiram-nivas <janakiram.bada@nivasinc.com>
2025-06-13 06:55:36 +0530

    terrafrom modules

--- terraform/modules/vms/main.tf
+++ terraform/modules/vms/main.tf
@@ -1,4 +1,73 @@
-provider "azure" {
+terraform {
+  backend "azurerm" {
+    resource_group_name = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.key
+    
+  } 
+}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "azurerm" {
   features {}
-  
+}
+
+resource "azure_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = azure_resource_group.rg.name
+}
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azure_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefixes
+}
+resource "azurerm_network_interface" "nic" {
+  count               = var.vm_count
+  name                = "${var.vm_name_prefix}-${count.index}"
+  location            = azure_resource_group.rg.location
+  resource_group_name = azure_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = var.vm_count
+  name                = "${var.vm_name_prefix}-${count.index}"
+  resource_group_name = azure_resource_group.rg.name
+  location            = azure_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
 }
